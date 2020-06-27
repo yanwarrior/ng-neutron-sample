340e0ee61e58c79e81183c36395ef29d3fdd0d36
Kong Bayan <yanwarsolah@gmail.com>
2020-06-28 02:38:18 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'neutron-labs';
+}
